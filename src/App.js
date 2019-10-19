c64e266ef1a282e23d11088ddca52d62352084f1
Guilherme Pendezza de Sousa <guilherme.sousa@loft.com.br>
2019-10-19 14:54:48 -0300

    capitulo 4

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Cabecalho from './components/cabecalho';
+
+function App() {
+  return (
+    <div>
+      <Cabecalho usuario="@guilhermependezza" />
+    </div>
+  );
+}
+
+export default App;
