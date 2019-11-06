7d718224b25322b31379e750a37e1d51abfe42da
Guilherme Pendezza de Sousa <guilherme.sousa@loft.com.br>
2019-11-06 16:56:08 -0300

    autenticando as rotas

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 
@@ -7,7 +7,7 @@ class Roteamento extends Component {
     render() {
         return (
             <Switch>
-                <Route path="/" component={HomePage} exact />
+                <PrivateRoute path="/" component={HomePage} exact />
                 <Route path="/login" component={LoginPage} />
             </Switch>
         )
@@ -15,3 +15,23 @@ class Roteamento extends Component {
 }
 
 export default Roteamento;
+
+class PrivateRoute extends Component {
+    estaAutenticado = () => {
+        if(localStorage.getItem('TOKEN')){
+            return true
+        } else {
+            return false;
+        }
+    }
+
+    render() {
+        const { component: Component, ...props } = this.props;
+
+        if(this.estaAutenticado()) {
+            return <Component {...props} />
+        } else {
+            return <Redirect to='/login' />
+        }
+    }
+}
